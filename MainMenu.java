08c44c1ca1c5b80435b34ee7de4de01bde9a6cde
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-24 03:25:15 -0500

    Update and rename Main.java to MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -5,21 +5,22 @@ public class MainMenu
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("How many students?");
-        int x = input.nextInt();
-        
-        System.out.println("How many classrooms?");
+
+        System.out.println("How many classrooms you want to add?");
         int n = input.nextInt();
-        
+        input.nextLine();// Consume newline
+
         School school = new School(n);
-        
+
         for (int i = 0; i < n; i++)
         {
-        	input.nextLine();
             System.out.println("Enter teacher's name for classroom " + (i + 1));
             String teacherName = input.nextLine();
-            
+
+            System.out.println("How many students in classroom " + (i + 1) + "?");
+            int x = input.nextInt();
+            input.nextLine();
+
             Student[] students = new Student[x];
             for (int j = 0; j < x; j++)
             {
@@ -27,21 +28,20 @@ public class MainMenu
                 String studentName = input.nextLine();
                 System.out.println("Enter student ID for student #" + (j + 1));
                 int studentId = input.nextInt();
-                input.nextLine(); // consume newline
+                input.nextLine();
                 students[j] = new Student(studentName, studentId);
             }
-            
+
             school.addClassroom(i, new Classroom(teacherName, students));
         }
-        
-        System.out.println("What is the name of the teacher?");
+
+        System.out.println("What is the name of the teacher of the student you are looking for?");
         String teacherName = input.nextLine();
-        
+
         System.out.println("What is the student ID?");
         int studentId = input.nextInt();
-        
+
         String result = school.findStudent(teacherName, studentId);
-        System.out.println(result);
-        
+        System.out.println("The student is " + result + ".");
     }
 }
