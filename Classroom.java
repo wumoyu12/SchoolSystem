0ac7e6ca295d7e2bee1897d482f9565bfd763ebd
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-24 02:23:53 -0500

    Create Classroom.java

---
+++ Classroom.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Classroom
+{
+    String teacherName;
+    Student[] students;
+    
+    public Classroom(String teacherName, Student[] students)
+    {
+        this.teacherName = teacherName;
+        this.students = students;
+    }
+}
