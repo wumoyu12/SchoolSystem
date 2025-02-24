e033072cfb19123766da3d62875ee875e8089322
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-24 02:22:12 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Student
+{
+    int sID;
+    String sName;
+    
+    public Student(String name, int id)
+    {
+        this.sName = name;
+        this.sID = id;
+    }
+}
