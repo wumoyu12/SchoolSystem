b86ecf29415e337cbb73ad5220d89a53bdede0ab
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-24 05:16:09 -0500

    Update MainMenu.java

--- MainMenu.java
+++ MainMenu.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
 
-public class MainMenu
-{
+public class MainMenu {
     public static void main(String[] args)
     {
         Scanner input = new Scanner(System.in);
 
         System.out.println("How many classrooms you want to add?");
-        int n = input.nextInt();
-        input.nextLine();// Consume newline
+        int n = CheckNum(input);
+        input.nextLine();
 
         School school = new School(n);
 
@@ -18,7 +17,7 @@ public class MainMenu
             String teacherName = input.nextLine();
 
             System.out.println("How many students in classroom " + (i + 1) + "?");
-            int x = input.nextInt();
+            int x = CheckNum(input);
             input.nextLine();
 
             Student[] students = new Student[x];
@@ -27,7 +26,7 @@ public class MainMenu
                 System.out.println("Enter student name for student #" + (j + 1));
                 String studentName = input.nextLine();
                 System.out.println("Enter student ID for student #" + (j + 1));
-                int studentId = input.nextInt();
+                int studentId = CheckidNum(input);
                 input.nextLine();
                 students[j] = new Student(studentName, studentId);
             }
@@ -39,9 +38,58 @@ public class MainMenu
         String teacherName = input.nextLine();
 
         System.out.println("What is the student ID?");
-        int studentId = input.nextInt();
+        int studentId = CheckidNum(input);
 
         String result = school.findStudent(teacherName, studentId);
         System.out.println("The student is " + result + ".");
+    }
+
+    private static int CheckNum(Scanner input)
+    {
+        while (true)
+        {
+            if (input.hasNextInt())
+            {
+                int value = input.nextInt();
+                if (value > 0)
+                {
+                    return value;
+                }
+                else
+                {
+                    System.out.println("Please enter the number that is equal or greater than 1.");
+                }
+            }
+            else
+            {
+                System.out.println("Please enter a valid number.");
+                input.next();
+            }
+        }
+    }
+
+    private static int CheckidNum(Scanner input)
+    {
+        while (true)
+        {
+            if (input.hasNextInt())
+            {
+                int value = input.nextInt();
+                String idString = Integer.toString(value);
+                if (idString.length() == 9)
+                {
+                    return value;
+                }
+                else
+                {
+                    System.out.println("Please enter a 9-digit ID Number.");
+                }
+            } 
+            else
+            {
+                System.out.println("Please enter a valid ID Number.");
+                input.next();
+            }
+        }
     }
 }
