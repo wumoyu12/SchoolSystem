94598c8cc15040de5bf7e96058130da915ff3812
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-02-24 03:28:03 -0500

    Update School.java

--- School.java
+++ School.java
@@ -1,57 +1,52 @@
-import java.util.Arrays;
-
 public class School
 {
-    Classroom[] classrooms;
-    
+    private Classroom[] classrooms;
+
     public School(int numClassrooms)
     {
         classrooms = new Classroom[numClassrooms];
     }
-    
+
     public void addClassroom(int index, Classroom classroom)
     {
         classrooms[index] = classroom;
     }
-    
+
     public String findStudent(String teacherName, int studentId)
     {
         Classroom classroom = null;
-        for (Classroom cls : classrooms)
-        {
-            if (cls.teacherName.equals(teacherName))
-            {
+        for (Classroom cls : classrooms) {
+            if (cls.getTeacherName().equals(teacherName)) {
                 classroom = cls;
                 break;
             }
         }
-        
+
         if (classroom == null)
         {
             return "Student Not Found";
         }
-        
-        Student[] students = classroom.students;
+
+        Student[] students = classroom.getStudents();
         int left = 0;
         int right = students.length - 1;
-        
+
         while (left <= right)
         {
             int mid = (left + right) / 2;
-            if (students[mid].sID == studentId)
+            if (students[mid].getStudentId() == studentId)
             {
-                return students[mid].sName;
-            } 
-            else if (students[mid].sID < studentId)
+                return students[mid].getStudentName();
+            }
+            else if (students[mid].getStudentId() < studentId)
             {
                 left = mid + 1;
-            } 
+            }
             else
             {
                 right = mid - 1;
             }
         }
-        
         return "Student Not Found";
     }
 }
